46fe236055ddc0e9e24128047f3afd57d41908b2
ataberk canitez <aticanitez@hotmail.com>
2025-06-10 12:55:09 +0300

    remove unnecessary main2 function

--- main.go
+++ main.go
@@ -2,30 +2,10 @@ package main
 
 import (
 	"github.com/ataberkcanitez/araqr/cmd"
-	"image/png"
-	"os"
-
-	"github.com/boombuler/barcode"
-	"github.com/boombuler/barcode/qr"
 )
 
 func main() {
 	if err := cmd.RunRootCmd(); err != nil {
 		panic(err)
 	}
-}
-
-func main2() {
-	// Create the barcode
-	qrCode, _ := qr.Encode("https://dev.randevumu.com/5-ataberk-hair-shop", qr.M, qr.Auto)
-
-	// Scale the barcode to 200x200 pixels
-	qrCode, _ = barcode.Scale(qrCode, 200, 200)
-
-	// create the output file
-	file, _ := os.Create("qrcode.png")
-	defer file.Close()
-
-	// encode the barcode as png
-	png.Encode(file, qrCode)
 }
